97765e6b82c7697881d0a70c263270c0c14ded41
gypang <gypang24@gmail.com>
2018-02-06 15:37:11 -0800

    Initial Commit

---
+++ flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Godwin Pang on 2/5/18.
+//  Copyright © 2018 godwinpang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
